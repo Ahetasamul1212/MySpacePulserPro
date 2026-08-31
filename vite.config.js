c0788958b70ee561871272c527c0983abcc603f0
unknown <turjo0599@gmail.com>
2026-08-31 17:15:48 +0600

    add deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,5 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  server: {
-    port: 5173,
-    open: true
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: false
-  }
+  base: 'https://github.com/Ahetasamul1212/MySpacePulserPro',
 })
